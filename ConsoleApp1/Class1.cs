41c2198d154ab37ca6b29601ef58abc2a812470f
lawesen <lawesen@163.com>
2020-03-13 10:22:28 +0800

    同个命名空间中的类引用
    新增

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public double a, b;
+        public double TestName()
+        {
+            return a * b;
+        }
+        public void Display()
+        {
+            Console.WriteLine("a的值是：{0}", a);
+            Console.WriteLine("b的值是：{0}", b);
+            Console.WriteLine("a*b的值是：{0}", TestName());
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 第一个示例
+/// </summary>
+/// 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 r = new Class1();
+            r.a = 100.1;
+            r.b = 102.11;
+            r.Display();
+            Console.ReadLine();
+            
+        }
+    }
+}
